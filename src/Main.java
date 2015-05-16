40a48a3e34f7bf091d25a7f447143ac6663b2957
matthias <h1395010@connect.hku.hk>
2015-05-17 00:16:11 +0800

    funny prototype

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,61 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
-/**
- * Created by matthias on 5/16/15.
- */
 public class Main
 {
+	static HashMap<String, Boolean>  de_map = new HashMap<String, Boolean>();
+	static HashMap<String, Boolean>  fr_map = new HashMap<String, Boolean>();
+	static HashMap<String, Boolean>  ru_map = new HashMap<String, Boolean>();
+	static HashMap<String, Boolean> eng_map = new HashMap<String, Boolean>();
 
     public static void main(String[] args) throws IOException
     {
         ArrayList<File> sub_dirs = new ArrayList<File>();
 
-        final String start_dir = "/home/matthias/Desktop/language_detective/word_lists_1";
-        listf( start_dir, sub_dirs );
+        final String filePath = "/home/matthias/Desktop/language_detective/word_lists_2";
+
+        listf( filePath, sub_dirs );
 
         for(File dir : sub_dirs)
         {
+        	String word_holding_directory_path = dir.toString().toLowerCase();
+        	
+
+        	
             BufferedReader br = new BufferedReader(new FileReader( dir ));
             String line = null;
             while ((line = br.readLine()) != null)
             {
-                System.out.println(line);
+                //System.out.println(line);
+                if(word_holding_directory_path.toLowerCase().contains("/de/") )
+                {
+                	de_map.put(line, false);	
+                }
+                if(word_holding_directory_path.toLowerCase().contains("/ru/") )
+                {
+                	ru_map.put(line, false);
+                }
+                if(word_holding_directory_path.toLowerCase().contains("/fr/") )
+                {
+                	fr_map.put(line, false);
+                }
+                if(word_holding_directory_path.toLowerCase().contains("/eng/") )
+                {
+                	eng_map.put(line, false);
+                }
             }
         }
+        
+        //print debugging
+        for (Map.Entry entry : de_map.entrySet()) 
+        {
+            System.out.println(entry.getKey() + ", " + entry.getValue());
+        }
+        
 
     }
 
@@ -53,6 +84,9 @@ public class Main
 
 
 }
+
+
+
 
 
 
