f2a660fe5bc5167aa85d5bbe48392e10c8ed2343
matthias <h1395010@connect.hku.hk>
2015-05-17 17:39:15 +0800

    proteus functionality

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,59 @@
-
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URISyntaxException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-
+import java.util.Scanner;
 
 public class Main
 {
+	
 	static HashMap<String, Boolean>  de_map = new HashMap<String, Boolean>();
 	static HashMap<String, Boolean>  fr_map = new HashMap<String, Boolean>();
 	static HashMap<String, Boolean>  ru_map = new HashMap<String, Boolean>();
 	static HashMap<String, Boolean> eng_map = new HashMap<String, Boolean>();
-
-    public static void main(String[] args) throws IOException
+	
+	
+    public static void main( String[] args ) throws URISyntaxException, IOException
     {
-        ArrayList<File> sub_dirs = new ArrayList<File>();
+        ArrayList<Path> dictionary_files = new ArrayList<Path>();
+    	
 
-        final String filePath = "/home/matthias/Desktop/language_detective/word_lists_2";
-
-        listf( filePath, sub_dirs );
-
-        for(File dir : sub_dirs)
+        File currentDir = new File( "." ); // Read current file location
+        //System.out.println(currentDir.getAbsolutePath());
+        
+        File targetDir = null;
+        if (currentDir.isDirectory()) 
         {
+            targetDir = new File( currentDir, "word_lists_2" ); // Construct the target directory file with the right parent directory
+        }
+        if ( targetDir != null && targetDir.exists() )
+        {
+            SearchDirectories.listDirectoryAndFiles( targetDir.toPath(), dictionary_files );
+        }
+        
+
+        for(Path dir : dictionary_files)
+        {
+        	//System.out.println( dir );
+        	
         	String word_holding_directory_path = dir.toString().toLowerCase();
         	
 
         	
-            BufferedReader br = new BufferedReader(new FileReader( dir ));
+            BufferedReader br = new BufferedReader( new FileReader( dir.toString() ) );
             String line = null;
             while ((line = br.readLine()) != null)
             {
@@ -53,41 +77,77 @@ public class Main
             }
         }
         
+        
         //print debugging
         for (Map.Entry entry : de_map.entrySet()) 
         {
             System.out.println(entry.getKey() + ", " + entry.getValue());
         }
         
-
-    }
-
-    public static void listf(String directoryName, ArrayList<File> files)
-    {
-        File directory = new File(directoryName);
-
-        // get all the files from a directory
-        File[] fList = directory.listFiles();
-
-        for (File file : fList)
+        
+        
+        //GET THE USER INPUT
+        ArrayList<String> input_sentence = new ArrayList<String>();
+        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a sentence: ");
+         
+        String [] tokens = in.nextLine().split("\\s");
+         
+        for (int i = 0; i < tokens.length; i++)
         {
-            if (file.isFile())
-            {
-                files.add(file);
-            }
-            else if (file.isDirectory())
-            {
-                listf(file.getAbsolutePath(), files);
-            }
+        	input_sentence.add( tokens[i].toString() );
         }
+         
+        if (input_sentence.size() == 0)
+        {
+           System.out.println("Insufficient input.");
+   
+        } 
+        else
+        {
+            for(String elem : input_sentence)
+            {
+                //System.out.println( elem );
+            }
+        
+        }
+       
+        
+        Iterator it = de_map.entrySet().iterator();
+        while (it.hasNext()) 
+        {
+            Map.Entry pair = (Map.Entry)it.next();
+            System.out.println(pair.getKey() + ", " + pair.getValue());
+
+            for(String elem : input_sentence)
+            {
+                if(pair.getKey().toString().toLowerCase().trim().equals( elem.toLowerCase().trim() ) )
+                {
+                	de_map.put(pair.getKey().toString(), true);
+                }
+//                	System.out.println( "pair.getKey().toString().toLowerCase().trim():    " + pair.getKey().toString().toLowerCase().trim() );              	
+//                	System.out.println( "elem.toLowerCase().trim():    " + elem.toLowerCase().trim() );
+
+            }
+            
+        }
+        
+        
+        //print debugging
+        System.out.println("post");
+        for (Map.Entry entry : de_map.entrySet()) 
+        {
+            System.out.println(entry.getKey() + ", " + entry.getValue());
+        }
+
+        
+        
     }
 
+
+    
+
+    
 
 }
-
-
-
-
-
-
-
